6ee18dc963709be82017018613dd4d7d39192f15
Yaroslav Zelepukhin <yarzeltan@gmail.com>
2026-04-20 21:03:40 +0300

    Принятая 2ая лаба СД

---
+++ labs/lab2/tests/test_B32.cpp
@@ -0,0 +1,60 @@
+#include "../src/base.hpp"
+#include <iostream>
+#include <cstdio>
+#include <cassert>
+#include <cstdlib>
+
+int main() {
+    const int SIZE = 50000;
+    const int CHUNK = 256;
+
+    char* data = new char[SIZE];
+    for (int i = 0; i < SIZE; ++i) {
+        data[i] = rand() % 256;
+    }
+
+    {
+        Base32File f("test_B32.bin", "wb");
+
+        int pos = 0;
+        while (pos < SIZE) {
+            int sz = CHUNK;
+            if (pos + sz > SIZE) {
+                sz = SIZE - pos;
+            }
+            f.write(data + pos, sz);
+            pos += sz;
+        }
+    }
+
+    char* read_data = new char[SIZE];
+
+    {
+        Base32File f("test_B32.bin", "rb");
+
+        int pos = 0;
+        while (pos < SIZE) {
+            int n = f.read(read_data + pos, CHUNK);
+            if (n == 0) break;
+            pos += n;
+        }
+
+        assert(pos == SIZE);
+    }
+
+    for (int i = 0; i < SIZE; ++i) {
+        if (data[i] != read_data[i]) {
+            std::cout << "Ошибка на позиции " << i << "\n";
+            delete[] data;
+            delete[] read_data;
+            return 1;
+        }
+    }
+
+    std::cout << "Base32 тест пройден\n";
+
+    delete[] data;
+    delete[] read_data;
+
+    return 0;
+}
